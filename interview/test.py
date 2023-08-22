2ce23b0059e64dc79d6632b04a0168dd535a02d3
faizalfeng <fzx401@gmail.com>
2023-08-22 16:18:36 +0800

    增加翻译模型

--- interview/test.py
+++
@@ -1,9 +0,0 @@
-def func(string: str):
-    max_step = len(string) // 2
-    dp1 = [string] * max_step
-    dp2 = [string] * max_step
-    for i in range(max_step):
-        dp1[i] = string[i + 1:] + string[i]
-        
-    
-    
