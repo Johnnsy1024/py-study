a8764a0c914462a804aa12bd4e179360566950de
faizalfeng <fzx401@gmail.com>
2023-08-25 16:17:02 +0800

    2023.8.25

--- data_structure_and_algorithm/logging/config.py
+++ data_structure_and_algorithm/logging/config.py
@@ -4,19 +4,31 @@ from datetime import datetime
 import json
 
 
-#
-# with open('log_config.json', 'r') as f:
-#     log_config = json.load(f)
-tmp = os.path.join(os.path.abspath(__file__))
-log_file_name = f"Log {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.log"
-# log_config['handlers']['file_handler']['filename'] = log_file_name
+def read_log_config(log_conf_dir: str) -> dict:
+    with open(log_conf_dir, 'r') as f:
+        log_conf = json.load(f)
+    return log_conf
 
-logging.config.dictConfig(log_config)
 
-# # 现在您可以在代码中使用日志记录
-logger = logging.getLogger()
-logger.debug("这是调试信息")
-logger.info("这是信息日志")
-logger.warning("这是警告信息")
-logger.error("这是错误信息")
-logger.critical("这是严重错误信息")
+def save_log_dir() -> str:
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    log_file_path = os.path.join(cur_dir, 'log_file')
+
+    return log_file_path
+
+
+if __name__ == '__main__':
+    log_file_name = f"Log {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.log"
+    log_file_dir = os.path.join(save_log_dir(), log_file_name)
+    log_config = read_log_config('log_config/log_config.json')
+    # Dynamically assign log file name with datetime
+    log_config['handlers']['file_handler']['filename'] = log_file_dir
+    # set relative log configuraion
+    logging.config.dictConfig(log_config)
+
+    logger = logging.getLogger()
+    logger.debug("这是调试信息")
+    logger.info("这是信息日志")
+    logger.warning("这是警告信息")
+    logger.error("这是错误信息")
+    logger.critical("这是严重错误信息")
