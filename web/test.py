8f167d1b9aa16c6b45dee29b8b9c15094f547dd2
zx.feng <zx.feng@zuche.com>
2024-02-04 20:09:50 +0800

    整理py_sytudy

--- web/test.py
+++ web/test.py
@@ -12,11 +12,14 @@ class ModelName(str, Enum):
 app = FastAPI()
 
 
+<<<<<<< HEAD
 @app.get("/")
 async def say_hello():
     return "I love u"
 
 
+=======
+>>>>>>> c0946524a359dd872d2670922f8cf76bcf47483a
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -26,8 +29,11 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+<<<<<<< HEAD
 
 
 @app.put("/items/{model_name}")
 def update_model(model_name: str, model: ModelName):
     return {"model_name": model_name}
+=======
+>>>>>>> c0946524a359dd872d2670922f8cf76bcf47483a
