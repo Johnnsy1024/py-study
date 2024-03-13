3a9a51fd0519e75b12561f4a3ff40cb0f0bfdbb3
FaizalFeng <fzx401@gmail.com>
2024-03-13 18:12:35 +0800

    20240313 完善python高级知识

--- python/partial_func.py
+++ python/partial_func.py
@@ -1,5 +1,16 @@
+"""
+Author: FaizalFeng fzx401@gmail.com
+Date: 2024-02-04 20:01:23
+LastEditors: FaizalFeng fzx401@gmail.com
+LastEditTime: 2024-03-13 17:23:32
+Copyright (c) 2024 by FaizalFeng, All Rights Reserved.
+"""
+
 from functools import partial
 
-new_int = partial(int, base=2)
 
-print(new_int("101"))
+def speak(name: str):
+    return f"hello, {name}"
+
+
+print(partial(speak, "Tom")())
