9eafe34977481a557b441a2a1eb3d367bc9e802d
FaizalFeng <fzx401@gmail.com>
2024-03-14 17:59:33 +0800

    20240314 更新了桶排序的递归写法

--- data_structure_and_algorithm/algorithm/sort/bucket_sort.py
+++ data_structure_and_algorithm/algorithm/sort/bucket_sort.py
@@ -1,7 +1,8 @@
 from random import randint
 
-def bucket_sort(nums : list, bucket_num: int):
-    max_val, min_val = -10000, 10000 
+
+def bucket_sort(nums: list, bucket_num: int):
+    max_val, min_val = -10000, 10000
     for i in nums:
         max_val = i if max_val < i else max_val
         min_val = i if min_val > i else min_val
@@ -11,13 +12,14 @@ def bucket_sort(nums : list, bucket_num: int):
         index = int((i - min_val) / bucket_range)
         if index == bucket_num:
             index -= 1
-        bucket[index].append(i)  
+        bucket[index].append(i)
     res = []
     for b in bucket:
         b.sort()
         res.extend(b)
     return res
 
+
 arr = [randint(0, 10) for _ in range(10)]
-print('before sort {}'.format(arr))
-print('after sort {}'.format(bucket_sort(arr, 10)))
+print("before sort {}".format(arr))
+print("after sort {}".format(bucket_sort(arr, 10)))
